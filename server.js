f9264e440de999617f63aac681b10f7575678d1b
Weranry <107329751+Weranry@users.noreply.github.com>
2024-11-03 15:56:10 +0800

    s

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 
 // 在全局作用域中加载字体
-PImage.registerFont('./font/simhei.ttf', 'SimHei');
-const font = PImage.registerFont('./font/simhei.ttf', 'SimHei');
+PImage.registerFont('./simhei.ttf', 'SimHei');
+const font = PImage.registerFont('./simhei.ttf', 'SimHei');
 font.loadSync(); // 同步加载字体
 
 app.get('/getlunarimg', (req, res) => {
