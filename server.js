0ac953621e199627976ae1c937a3ccb702ef721d
Weranry <107329751+Weranry@users.noreply.github.com>
2024-12-11 22:13:32 +0800

    Q

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = 3000;
 const moonImgApi = require('./moonImgApi');
 const lunarImgApi = require('./lunarImgApi'); // Added import for lunarImgApi
 const scheduleApi = require('./scheduleApi'); // Added import for scheduleApi
+const getDatesWithOffset = require('./time.js');
+
+app.get('/dates', (req, res) => {
+    const result = getDatesWithOffset();
+    res.json(result);
+});
 
 
 // other routes...
@@ -12,6 +18,8 @@ const scheduleApi = require('./scheduleApi'); // Added import for scheduleApi
 app.use(lunarImgApi);
 app.use(scheduleApi);
 app.use(moonImgApi);
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
