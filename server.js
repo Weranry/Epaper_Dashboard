24b9082e1f4449c0825a89e07b8c04d8f3b4ed28
Weranry <107329751+Weranry@users.noreply.github.com>
2024-12-16 20:52:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const port = 3000;
 const moonImgApi = require('./moonImgApi');
 const lunarImgApi = require('./lunarImgApi'); // Added import for lunarImgApi
 const scheduleApi = require('./scheduleApi'); // Added import for scheduleApi
-
+const moonImgApi = require('./sunImgApi');
 
 // 使用路由
 app.use(lunarImgApi);
 app.use(scheduleApi);
 app.use(moonImgApi);
-
+app.use(sunImgApi);
 
 
 app.listen(port, () => {
