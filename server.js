7368b177bf3d436c509817eb2a17532c8747627a
Weranry <107329751+Weranry@users.noreply.github.com>
2024-12-19 18:54:46 +0800

    又修复了一个大小写问题

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const moonImgApi = require('./moonImgApi');
 const lunarImgApi = require('./lunarImgApi'); // Added import for lunarImgApi
 const scheduleApi = require('./scheduleApi'); // Added import for scheduleApi
 const sunImgApi = require('./sunImgApi');
-const WeatherImgApi = require('./weatherApi');
+const WeatherImgApi = require('./weatherAPI');
 // 使用路由
 app.use(lunarImgApi);
 app.use(scheduleApi);
